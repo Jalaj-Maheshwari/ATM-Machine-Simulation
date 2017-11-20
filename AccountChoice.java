ee1cafc837e1c4e02af104408026b6d8cc211b14
Jalaj Maheshwari <jalaj.1702@gmail.com>
2017-11-20 21:42:13 +0530

    Update AccountChoice.java

--- AccountChoice.java
+++ AccountChoice.java
@@ -6,10 +6,11 @@ import java.sql.*;
 import java.util.*;
 
 public class AccountChoice extends JFrame implements ActionListener {    
+   
    JFrame frame;
-   JLabel lbl_option,lbl_img,lbl_name; 
+   JLabel lbl_option, lbl_img, lbl_name; 
    JPanel panel;
-   JButton btn_savings,btn_current;
+   JButton btn_savings, btn_current;
    Container co;    
    Connection con;
    Statement st;
@@ -20,72 +21,72 @@ public class AccountChoice extends JFrame implements ActionListener {
    public static int type;
    ResultSet rs;
 
-public AccountChoice(String str){
+   public AccountChoice(String str){
           
-   val=str;
+      	val = str;
 
-   frame=new JFrame();
-   frame.setSize(600,600);
-   frame.setTitle(" ACCOUNT CHOICE ");
+      	frame = new JFrame();
+      	frame.setSize(600,600);
+      	frame.setTitle(" ACCOUNT CHOICE ");
 
-   co= getContentPane();
-   JPanel panel=new JPanel();
+      	co = getContentPane();
+      	JPanel panel = new JPanel();
 
-   panel.setLayout(null);
-   lbl_option=new JLabel("PLEASE  SELECT  AN  ACCOUNT  FOR  TRANSACTION");
-   lbl_option.setFont(new Font("Perpetua",Font.BOLD,22));
-   lbl_option.setSize(550,100);
-   lbl_option.setLocation(40,200);
-   panel.add(lbl_option);
+   		panel.setLayout(null);
+   		lbl_option=new JLabel("PLEASE  SELECT  AN  ACCOUNT  FOR  TRANSACTION");
+   		lbl_option.setFont(new Font("Perpetua",Font.BOLD,22));
+   		lbl_option.setSize(550,100);
+   		lbl_option.setLocation(40,200);
+   		panel.add(lbl_option);
 
-   btn_current=new JButton("Current Account");
-   btn_current.setFont(new Font("Perpetua",Font.BOLD,18));
-   btn_current.setSize(200,50);
-   btn_current.setLocation(70,300);
-   btn_current.addActionListener(this);
-   panel.add(btn_current);
+   		btn_current = new JButton("Current Account");
+   		btn_current.setFont(new Font("Perpetua",Font.BOLD,18));
+   		btn_current.setSize(200,50);
+   		btn_current.setLocation(70,300);
+   		btn_current.addActionListener(this);
+   		panel.add(btn_current);
 
-   btn_savings=new JButton("Savings Account");
-   btn_savings.setFont(new Font("Perpetua",Font.BOLD,18));
-   btn_savings.setSize(200,50);
-   btn_savings.setLocation(320,300);
-   btn_savings.addActionListener(this);
-   panel.add(btn_savings);
-//panel.setBackground(Color.orange);
+   		btn_savings = new JButton("Savings Account");
+   		btn_savings.setFont(new Font("Perpetua",Font.BOLD,18));
+   		btn_savings.setSize(200,50);
+   		btn_savings.setLocation(320,300);
+   		btn_savings.addActionListener(this);
+   		panel.add(btn_savings);
 
-ImageIcon ig=new ImageIcon("atmimage.png");
-lbl_img=new JLabel(ig);
-lbl_img.setSize(500,100);
-lbl_img.setLocation(50,25); 
-panel.add(lbl_img);       
+		ImageIcon ig = new ImageIcon("atmimage.png");
+		lbl_img = new JLabel(ig);
+		lbl_img.setSize(500,100);
+		lbl_img.setLocation(50,25); 
+		panel.add(lbl_img);       
 
-  try { 
-          String name=null;
+  		try { 
+          
+			String name = null;
                          
-          Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
-          con = DriverManager.getConnection("Jdbc:Odbc:atmdb");
-          String query = "select name from currentCustomers  where pin=?";
+          	Class.forName("sun.jdbc.odbc.JdbcOdbcDriver");
+          	con = DriverManager.getConnection("Jdbc:Odbc:atmdb");
+          	String query = "select name from currentCustomers where pin = ?";
 
-          ps = con.prepareStatement(query); // create a statement
-          ps.setString(1,val);   // set input parameter
-          rs = ps.executeQuery();      
+          	ps = con.prepareStatement(query); // create a statement
+          	ps.setString(1,val);   // set input parameter
+          	rs = ps.executeQuery();      
           
-          while(rs.next()){                    
-                str_name=rs.getString("name");
-          }
+          	while(rs.next()){                    
+                str_name = rs.getString("name");
+          	}
                             
-        lbl_name = new JLabel("HELLO  "+str_name);
-        rs=null;
-        String query1 = "select name from savingsCustomers  where pin=?";
+        	lbl_name = new JLabel("HELLO  " + str_name);
+        	rs = null;
+        	String query1 = "select name from savingsCustomers where pin = ?";
 
-        ps = con.prepareStatement(query1); // create a statement
-        ps.setString(1,val);   // set input parameter
-        rs = ps.executeQuery();      
-        while(rs.next()){                    
+        	ps = con.prepareStatement(query1); // create a statement
+        	ps.setString(1,val);   // set input parameter
+            rs = ps.executeQuery();      
+        	while(rs.next()){                    
                 str_name=rs.getString("name");
-        }
-        lbl_name = new JLabel("HELLO  "+str_name);
-        lbl_name.setSize(400,50);
+        	}
+        	lbl_name = new JLabel("HELLO  " + str_name);
+        	lbl_name.setSize(400,50);
         lbl_name.setLocation(140,150);
         lbl_name.setVisible(true);
         lbl_name.setFont(new Font("Perpetua",Font.BOLD,22));  
